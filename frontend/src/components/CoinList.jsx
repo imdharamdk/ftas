b430f4a4781f8c422b9cc2b905fb16253d8d7ff5
Dharmender Kumar <97335824+imdharamdk@users.noreply.github.com>
2026-03-06 13:37:46 +0530

    Update CoinList.jsx

--- frontend/src/components/CoinList.jsx
+++ frontend/src/components/CoinList.jsx
@@ -14,7 +14,7 @@ loadCoins();
 async function loadCoins(){
 
 const res=await axios.get(
-"https://ftas-api.onrender.com/api/coins/volume"
+"https://ftas.onrender.com/api/coins/volume"
 );
 
 setCoins(res.data.slice(0,30));
